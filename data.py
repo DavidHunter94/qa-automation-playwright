8c1f525144afa25598c113bca5244efd6f7d2dab
Victor David Martinez Matias <davidmatiasmh94@gmail.com>
2026-03-22 20:22:20 -0600

    fix: actualizar URL del servidor Urban Routes

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-0d28e7ab-9ebc-4ccc-9073-63138a4809e0.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-d2f4eba1-40c9-468b-ac50-05da83d312ed.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
